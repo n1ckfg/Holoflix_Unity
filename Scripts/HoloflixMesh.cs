221568842c7fedc3f94726793daf7e506d24f53f
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-21 12:34:03 -0400

    edit main script

--- Scripts/HoloflixMesh.cs
+++ Scripts/HoloflixMesh.cs
@@ -21,7 +21,7 @@ public class HoloflixMesh : MonoBehaviour {
 	[Tooltip ("The pixel width/height of the color portion of the movie.")]
 	public Vector2 rgbDim;
 
-	//public Material depthMovieMaterial;
+	[HideInInspector] public Material depthMovieMaterial;
 
 	public float depth = 1f;
 	public float persp = 1f;
@@ -73,21 +73,20 @@ public class HoloflixMesh : MonoBehaviour {
 	int _currentResY = 0;
 	Shader _lastShader = null;
 
-	void Start()
-	{
+	private void Awake() {
+		if (depthMovieMaterial == null) depthMovieMaterial = GetComponent<Renderer>().sharedMaterial;
+	}
+
+	private void Start() {
 		checkShaders (); //just for safety.
 		
-		if (Application.isPlaying)
-		{
-			if (!movie) 
-			{
+		if (Application.isPlaying) {
+			if (!movie)  {
 				Renderer r = GetComponent<Renderer>();
-				if (r)
-					movie = (MovieTexture)r.material.mainTexture;
+				if (r) movie = (MovieTexture) r.material.mainTexture;
 			}
 
-			if (movie) 
-			{
+			if (movie) {
 				movie.Play ();
 				audioSrc = GetComponent<AudioSource> ();
 				audioSrc.clip = movie.audioClip;
@@ -99,34 +98,24 @@ public class HoloflixMesh : MonoBehaviour {
 	}
 
 
-	public void play()
-	{
-		if (movie)
-			movie.Play();
-		if (audioSrc)
-			audioSrc.Play ();
-
+	public void play() {
+		if (movie) movie.Play();
+		if (audioSrc) audioSrc.Play ();
 	}
-	public void stop()
-	{
-		if (movie) 
-		{
+
+	public void stop() {
+		if (movie) {
 			movie.Stop (); //this leaves it at the current frame. when what it should really do is reset to frame 1
 			StartCoroutine("stopping");
 			audioSrc.Stop ();
 		}
 	}
-	public void pause()
-	{
-		if (movie)
-			movie.Pause();
-		if (audioSrc)
-			audioSrc.Pause ();
+	public void pause() {
+		if (movie) movie.Pause();
+		if (audioSrc) audioSrc.Pause ();
 	}
 		
-
-	IEnumerator stopping()
-	{
+	IEnumerator stopping() {
 		yield return new WaitForSeconds (.1f);
 		movie.Play ();
 		yield return new WaitForSeconds (.1f);
@@ -134,39 +123,37 @@ public class HoloflixMesh : MonoBehaviour {
 
 	}
 		
-	public void sliderChanged()
-	{
+	public void sliderChanged() {
 		GetComponent<Renderer>().sharedMaterial.SetFloat("_Displacement", depth);//Slider.value);
 		GetComponent<Renderer>().sharedMaterial.SetFloat("_ForcedPerspective", persp);//Slider.value);
 		GetComponent<Renderer>().sharedMaterial.SetFloat ("_ParticleSize", size);//Slider.value);
 	}
 
-	void updateSettings()
-	{
+	private void updateSettings() {
 		meshResX = Mathf.Clamp (meshResX, 1, 255);
 		meshResY = Mathf.Clamp (meshResY, 1, 254);
 
-		while (meshResX + meshResY > 253)//this will create too many verts 
-		{ 
+		//this will create too many verts 
+		while (meshResX + meshResY > 253) { 
 			Debug.Log ("Reducing the mesh resolution, too many verts would be created."); //remember unity can only have 64k verts on a mesh
 			meshResX--;
 		}
 
 		//populate all the arrays outside of the mesh update loop to avoid tons of GC allocation
 		int vertCount = (meshResX + 1) * (meshResY + 1); //+1 is inclusive since we need verts at the end of the count to complete the quads
-		if (graphicsShaderLevel == 1)
-			vertCount = vertCount * 4; //we need to account for our own verts here, since our GPU does not support the needed shader
+		//we need to account for our own verts here, since our GPU does not support the needed shader
+		if (graphicsShaderLevel == 1) vertCount = vertCount * 4; 
 
 		int triangleCount =  meshResX * meshResY * 6;
-		if (graphicsShaderLevel == 2)
-			triangleCount = meshResX * meshResY * 3;
+		if (graphicsShaderLevel == 2) triangleCount = meshResX * meshResY * 3;
 
 		verts = new List<Vector3> (); 
 		uvs = new List<Vector2> ();
 		uv2s = new List<Vector2> ();
 		normals = new List<Vector3> ();
-		for (int v = 0; v < vertCount; v++) //populate the list.. verts are required in this format due to the way the mesh takes in verts as an argument
-		{
+
+		//populate the list.. verts are required in this format due to the way the mesh takes in verts as an argument
+		for (int v = 0; v < vertCount; v++) {
 			verts.Add (Vector3.zero);
 			uvs.Add (new Vector2());
 			uv2s.Add (new Vector2());
@@ -178,55 +165,45 @@ public class HoloflixMesh : MonoBehaviour {
 		_currentResY = meshResY;
 	}
 
-	void OnValidate()
-	{
+	private void OnValidate() {
 		checkShaders ();
 		updateSettings ();
 	}
 
-	void checkShaders()
-	{
+	private void checkShaders() {
 		//Detect whether current Graphics Shader Level supports Geometry shaders
 		//manually set the fallbacks so we can detect that the fallback occured and adjust our geometry accordingly
 		Material m = GetComponent<Renderer>().sharedMaterial;
-		if (!m) 
-		{
-			return;
-		}
+		if (!m) return;
+
 		currentShader = m.shader;
-		if (currentShader == _lastShader) //nothing to do here.
-			return;
+		//nothing to do here.
+		if (currentShader == _lastShader) return;
 
 		/*
-        if (currentShader == particleShader && !particleShader.isSupported) 
-		{
+        if (currentShader == particleShader && !particleShader.isSupported) {
 			m.shader = particleFallbackShader;
 			currentShader = particleFallbackShader;
 		}
-		if (currentShader == particleAdditiveShader && !particleAdditiveShader.isSupported) 
-		{
+		if (currentShader == particleAdditiveShader && !particleAdditiveShader.isSupported) {
 			m.shader = particleAdditiveFallbackShader;
 			currentShader = particleAdditiveFallbackShader;
 		}
-		if (currentShader == particleCutoutShader && !particleCutoutShader.isSupported) 
-		{
+		if (currentShader == particleCutoutShader && !particleCutoutShader.isSupported) {
 			m.shader = particleCutoutFallbackShader;
 			currentShader = particleCutoutFallbackShader;
 		}
 
 		//if even the fallback is bad, then fall back to the default holovid shader.
-		if (currentShader == particleFallbackShader && !particleFallbackShader.isSupported) 
-		{
+		if (currentShader == particleFallbackShader && !particleFallbackShader.isSupported) {
 			m.shader = holovidShader;
 			currentShader = holovidShader;
 		}
-		if (currentShader == particleAdditiveFallbackShader && !particleAdditiveFallbackShader.isSupported) 
-		{
+		if (currentShader == particleAdditiveFallbackShader && !particleAdditiveFallbackShader.isSupported) {
 			m.shader = holovidShader;
 			currentShader = holovidShader;
 		}
-		if (currentShader == particleCutoutFallbackShader && !particleCutoutFallbackShader.isSupported) 
-		{
+		if (currentShader == particleCutoutFallbackShader && !particleCutoutFallbackShader.isSupported) {
 			m.shader = holovidShader;
 			currentShader = holovidShader;
 		}
@@ -235,12 +212,13 @@ public class HoloflixMesh : MonoBehaviour {
 		//shader level 1 means the GPU can't support cutting up the mesh, so we have to do the cutting in C# and the shader only pushes the verts around based on camera view.
 		//shader level 0 means we are just trying to use the simplest method, which is just vert displacement based on the depth movie texture values and material settings, with no mesh cutting
 
-		if (currentShader == particleShader || currentShader == particleAdditiveShader || currentShader == particleCutoutShader) 
+		if (currentShader == particleShader || currentShader == particleAdditiveShader || currentShader == particleCutoutShader) {
 			graphicsShaderLevel = 2;
-		else if (currentShader == particleFallbackShader || currentShader == particleAdditiveFallbackShader || currentShader == particleCutoutFallbackShader) 
+		} else if (currentShader == particleFallbackShader || currentShader == particleAdditiveFallbackShader || currentShader == particleCutoutFallbackShader) { 
 			graphicsShaderLevel = 1;
-		else
+		} else {
 			graphicsShaderLevel = 0;
+		}
         */
 
 		//Unity doesn't seem to set keyword shader values properly when switching materials
@@ -254,8 +232,7 @@ public class HoloflixMesh : MonoBehaviour {
 		_lastShader = currentShader;
 	}
 	
-	void Update()
-	{
+	private void Update() {
 		sliderChanged();
 
 		checkShaders ();
@@ -263,27 +240,20 @@ public class HoloflixMesh : MonoBehaviour {
 		meshResX = Mathf.Clamp (meshResX, 1, 255);
 		meshResY = Mathf.Clamp (meshResY, 1, 254); //maximum vert count limit
 		
-		if (meshResX != _currentResX || meshResY != _currentResY)
-			updateSettings ();
+		if (meshResX != _currentResX || meshResY != _currentResY) updateSettings ();
 
 
-		if (imageRes.x < 1)
-			imageRes.x = 1;
-		if (imageRes.y < 1)
-			imageRes.y = 1;
-		if (rgbDim.x < 1)
-			rgbDim.x = 1;
-		if (rgbDim.y < 1)
-			rgbDim.y = 1;
+		if (imageRes.x < 1) imageRes.x = 1;
+		if (imageRes.y < 1) imageRes.y = 1;
+		if (rgbDim.x < 1) rgbDim.x = 1;
+		if (rgbDim.y < 1) rgbDim.y = 1;
 
 		//David Lycan - Update the dimensions in the movie's material if necessary
 		Vector4 newDims = new Vector4( 1.07333f * rgbDim.y / imageRes.x, 1.09f * rgbDim.y / imageRes.y, 1f, 1f);
 		
-		if (previousDims != newDims)
-		{
-			if (graphicsShaderLevel > 0)
-			{
-				//depthMovieMaterial.SetVector("_Dims", newDims);
+		if (previousDims != newDims) {
+			if (graphicsShaderLevel > 0) {
+				depthMovieMaterial.SetVector("_Dims", newDims);
 			}
 			
 			previousDims = newDims;
@@ -297,18 +267,13 @@ public class HoloflixMesh : MonoBehaviour {
 			new Vector2 (rgbDim.x/imageRes.x, rgbDim.y/imageRes.y)); //uv dims - send whole and let the shader do the work here.
 	}
 
-
-
-
-	void generateHolovidMesh(GameObject g, Vector2 pos, Vector2 dims, Vector2 UVpos, Vector2 UVdims)
-	{
+	private void generateHolovidMesh(GameObject g, Vector2 pos, Vector2 dims, Vector2 UVpos, Vector2 UVdims) {
 
 
         //vert index
 		int v = 0;
         Vector2 uv = new Vector2();
-		for (int r = 0; r <= meshResY; r++)
-		{
+		for (int r = 0; r <= meshResY; r++) {
 			//lerp between the top left and bottom left, then lerp between the top right and bottom right, and save the vectors
 
 			float rowLerpValue = (float)r / (float)meshResY;
@@ -320,8 +285,7 @@ public class HoloflixMesh : MonoBehaviour {
 			cellLeft = Vector3.Lerp(pos, bottomLeft, rowLerpValue); //lerp between topleft/bottomleft
 			cellRight = Vector3.Lerp(topRight, bottomRight, rowLerpValue); //lerp between topright/bottomright
 
-			for (int c = 0; c <= meshResX; c++)
-			{
+			for (int c = 0; c <= meshResX; c++) {
 				//Now that we have our start and end coordinates for the row, iteratively lerp between them to get the "columns"
 				float columnLerpValue = (float)c / (float)meshResX;
 
@@ -344,10 +308,8 @@ public class HoloflixMesh : MonoBehaviour {
 
 				//David Lycan - When the Graphics Shader Level does not support Geometry shaders add the extra vertices, uvs and normals necessary
 				//				Also add triangles to form each billboard quad here
-				if (graphicsShaderLevel == 1)
-				{
-					if (v + 1 >= verts.Count) 
-					{
+				if (graphicsShaderLevel == 1) {
+					if (v + 1 >= verts.Count) {
 						//Debug.LogWarning ("Array sizes were not properly calculated in updateSettings()!?");
 						return;
 					}
@@ -379,12 +341,9 @@ public class HoloflixMesh : MonoBehaviour {
 		//we only want < gridunits because the very last verts in bth directions don't need triangles drawn for them.
 		//David Lycan - When the Graphics Shader Level does support Geometry shaders or the default Holovid shader is being used then add triangles to form the video geometry here
 
-		if (graphicsShaderLevel == 1)
-		{
-			for (int x = 0; x < meshResX; x++)
-			{
-				for (int y = 1; y <= meshResY; y++)
-				{
+		if (graphicsShaderLevel == 1) {
+			for (int x = 0; x < meshResX; x++) {
+				for (int y = 1; y <= meshResY; y++) {
 					triangles[t] = x * 4 + y * (meshResX + 1) * 4 + 1; t++;
 					triangles[t] = x * 4 + y * (meshResX + 1) * 4 + 2; t++;
 					triangles[t] = x * 4 + y * (meshResX + 1) * 4; t++;
@@ -395,19 +354,14 @@ public class HoloflixMesh : MonoBehaviour {
 					
 				}
 			}
-		}
-		else
-		{
-			for (int x = 0; x < meshResX; x++)
-			{
-				for (int y = 0; y < meshResY; y++)
-				{
+		} else {
+			for (int x = 0; x < meshResX; x++) {
+				for (int y = 0; y < meshResY; y++) {
 					triangles[t] = x + ((y + 1) * (meshResX + 1)); t++;
 					triangles[t] = (x + 1) + (y * (meshResX + 1)); t++;
 					triangles[t] = x + (y * (meshResX + 1)); t++; //width in verts
 
-					if (graphicsShaderLevel != 2)
-					{
+					if (graphicsShaderLevel != 2) {
 						triangles[t] = x + ((y + 1) * (meshResX + 1)); t++;
 						triangles[t] = (x + 1) + (y + 1) * (meshResX + 1); t++;
 						triangles[t] = (x + 1) + (y * (meshResX + 1)); t++;
@@ -419,16 +373,15 @@ public class HoloflixMesh : MonoBehaviour {
 
 		//now that we have the mesh ready to go lets put it in
 		MeshFilter meshFilter = g.GetComponent<MeshFilter>();
-		if (!meshFilter)
-			meshFilter = g.AddComponent<MeshFilter> ();
+		if (!meshFilter) meshFilter = g.AddComponent<MeshFilter> ();
 		meshFilter.sharedMesh.Clear ();
+
 		//David Lycan - Set Mesh method is now based on the Unity version
 		#if UNITY_VERSION_PRE_5_2
 			meshFilter.vertices = verts.ToArray();
 			meshFilter.triangles = triangles.ToArray();
 			meshFilter.uv = uvs.ToArray();
-			if (graphicsShaderLevel == 1)
-			{
+			if (graphicsShaderLevel == 1) {
 				meshFilter.uv2 = uv2s.ToArray();
 			}
 			meshFilter.normals = normals.ToArray();
@@ -436,8 +389,7 @@ public class HoloflixMesh : MonoBehaviour {
 			meshFilter.sharedMesh.SetVertices(verts);
 			meshFilter.sharedMesh.SetTriangles(triangles, 0, false); //don't recalculate bounds
 			meshFilter.sharedMesh.SetUVs(0, uvs);
-			if (graphicsShaderLevel == 1)
-			{
+			if (graphicsShaderLevel == 1) {
 				meshFilter.sharedMesh.SetUVs(1, uv2s);
 			}
 			meshFilter.sharedMesh.SetNormals(normals);
